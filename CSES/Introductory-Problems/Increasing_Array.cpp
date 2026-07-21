0039ccd0f92284da4758f9973df3d468b0a863eb
tanvir1607 <10viranik@gmail.com>
2026-07-21 12:12:39 +0600

    Increasing Array

---
+++ CSES/Introductory-Problems/Increasing_Array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main()
+{
+    optimize();
+
+    int size;
+    cin >> size;
+    vector<ll> vec(size);
+    for (ll &x : vec) cin >> x;
+
+    ll ans = 0;
+    ll mx = vec.front();
+    for (ll x : vec) mx = max(x, mx), ans += mx - x;
+    cout << ans << endl;
+
+    return 0;
+}
