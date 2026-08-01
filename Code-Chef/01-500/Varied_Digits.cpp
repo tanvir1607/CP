3e2239f37f222535376b27636594256cb3d048aa
tanvir1607 <10viranik@gmail.com>
2026-08-01 13:32:34 +0600

    Varied Digits

---
+++ Code-Chef/01-500/Varied_Digits.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main()
+{
+    optimize();
+
+    int num;
+    cin >> num;
+    cout << (num % 10 != num / 10 ? "Yes" : "No") << endl;
+
+    return 0;
+}
