de7b27d7754c40bc5f61c9c56b6dadf5314c38a0
tanvir1607 <10viranik@gmail.com>
2026-08-03 22:15:24 +0600

    Up-Down Palindrome

--- Code-Chef/03-1000-1400/Up_Down_Palindrome.cpp
+++ Code-Chef/03-1000-1400/Up_Down_Palindrome.cpp
@@ -1,3 +1,60 @@
+// Solution - 01
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+
+bool isPalindrome(vector<int> vec, int val)
+{
+    for (int &x : vec) x <= val ? x++ : x--;
+    for (int i = 0, j = vec.size() - 1; i <= j; i++, j--)
+        if (vec[i] != vec[j]) return false;
+    return true;
+}
+
+
+int main()
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size;
+        cin >> size;
+        vector<int> vec(size);
+        for (int &x : vec) cin >> x;
+
+        int val = -1;
+        for (int i = 0, j = size - 1; i < j; i++, j--)
+        {
+            int a = vec[i];
+            int b = vec[j];
+
+            if (a > b) swap(a, b);
+
+            if (a == b) continue;
+            else if (b - a == 2) val = a;
+            else  val = -2;
+            break;
+        }
+        if (val == -1) cout << "Yes" << endl;
+        else if (val == -2) cout << "No" << endl;
+        else cout << (isPalindrome(vec, val) || isPalindrome(vec, val + 1) ? "Yes" : "No") << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+// Solution - 02
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -19,25 +76,27 @@ int main()
         for (int &x : vec) cin >> x;
 
         bool flag = true;
-        ll L = INT_MIN;
-        ll R = INT_MAX;
-        for (int i = 0, j = size - 1; i <= j; i++, j--) 
+        int L = INT_MIN;
+        int R = INT_MAX;
+        for (int i = 0; i < size / 2; i++) 
         {
-            if (vec[i] == vec[j]) continue;
+            int a = vec[i];
+            int b = vec[size - i - 1];
+            if (a > b) swap(a, b);
 
-            ll small = min(vec[i], vec[j]);
-            ll large = max(vec[i], vec[j]);
-            if (large - small != 2) 
+            if (a == b) continue;
+            else if (b - a == 2) 
+            {
+                L = max(L, a);
+                R = min(R, b);
+            }
+            else
             {
                 flag = false;
                 break;
             }
-
-            L = max(L, small);
-            R = min(R, large);
         }
-        if (flag && L < R) cout << "Yes" << endl;
-        else cout << "No" << endl;
+        cout << ((flag && L < R) ? "Yes" : "No") << endl;
     }
 
     return 0;
