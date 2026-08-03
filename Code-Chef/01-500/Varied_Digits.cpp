9c78194f9d9714bde8235d30e9536be76ef248c5
tanvir1607 <10viranik@gmail.com>
2026-08-03 20:28:08 +0600

    Varied Digits

--- Code-Chef/01-500/Varied_Digits.cpp
+++ Code-Chef/01-500/Varied_Digits.cpp
@@ -1,3 +1,4 @@
+// Solution - 01
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,6 +13,29 @@ int main()
     int num;
     cin >> num;
     cout << (num % 10 != num / 10 ? "Yes" : "No") << endl;
+
+    return 0;
+}
+
+
+
+
+
+// Solution - 02
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main()
+{
+    optimize();
+
+    int num;
+    cin >> num;;
+    cout << (num % 11 ? "Yes" : "No") << endl;
 
     return 0;
 }
