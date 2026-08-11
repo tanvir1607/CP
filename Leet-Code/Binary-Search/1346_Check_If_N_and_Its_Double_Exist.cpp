451d62ecd124196095cceb2074b6001e308616a9
tanvir1607 <10viranik@gmail.com>
2026-08-11 13:26:32 +0600

    1346. Check If N and Its Double Exist

--- Leet-Code/Binary-Search/1346_Check_If_N_and_Its_Double_Exist.cpp
+++ Leet-Code/Binary-Search/1346_Check_If_N_and_Its_Double_Exist.cpp
@@ -1,3 +1,4 @@
+// Solution - 01
 class Solution {
 private:
     bool binary(vector<int> &vec, int target, int idx) {
@@ -22,6 +23,36 @@ public:
         sort(arr.begin(), arr.end());
         for (int i = 0; i < arr.size() - 1; i++) {
             if (binary(arr, 2 * arr[i], i)) return true;
+        }
+        return false;
+    }
+};
+
+
+
+
+
+// Solution - 02
+class Solution {
+private:
+    bool binary(vector<int> &vec, int target, int idx) {
+        int l = idx;
+        int r = vec.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (vec[mid] < target) l = mid + 1;
+            else if (vec[mid] > target) r = mid - 1;
+            else return true;
+        }
+        return false;
+    }
+
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr[i] >= 0 && binary(arr, 2*arr[i], i + 1)) return true;
+            else if (arr[i] % 2 == 0 && binary(arr, arr[i] / 2, i + 1)) return true;
         }
         return false;
     }
