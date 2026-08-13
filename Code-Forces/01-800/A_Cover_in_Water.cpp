dc8e58a43373a1b0018f9ec04c5ec36dfffec413
tanvir1607 <10viranik@gmail.com>
2026-08-13 12:41:20 +0600

    A. Cover in Water

---
+++ Code-Forces/01-800/A_Cover_in_Water.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int solve()
+{
+    int size;
+    string str;
+    cin >> size >> str;
+    for (int i = 0, j = 1, k = 2; k < size; i++, j++, k++)
+    {
+        if (str[i] == '.' && str[j] == '.' && str[k] == '.') 
+            return 2;
+    }
+    return count(str.begin(), str.end(), '.');
+}
+
+int main()
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        cout << solve() << endl;
+    }
+
+    return 0;
+}
