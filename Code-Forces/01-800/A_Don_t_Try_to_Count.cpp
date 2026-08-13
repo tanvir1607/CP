6f40e56d6f159311c51d9432d2304d2837493364
tanvir1607 <10viranik@gmail.com>
2026-08-13 15:47:22 +0600

    A. Don't Try to Count

---
+++ Code-Forces/01-800/A_Don_t_Try_to_Count.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main()
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int n, m;
+        string x, s;
+        cin >> n >> m >> x >> s;
+
+        int ans = -1;
+        for (int i = 0; i < 6 && ans == -1; i++, x += x)
+        {
+            if (x.find(s) != string::npos) ans = i;
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
