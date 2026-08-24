d7f019441aea202dceb497bff190fbc1a925531b
tanvir1607 <10viranik@gmail.com>
2026-08-24 11:06:38 +0600

    B. Blank Space

---
+++ Code-Forces/01-800/B_Blank_Space.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main()
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size;
+        cin >> size;
+        vector<int> vec(size);
+        for (int &x : vec) cin >> x;
+
+        int ans = 0, cnt = 0;
+        for (int x : vec)
+        {
+            x ? cnt = 0 : cnt++;
+            ans = max(cnt, ans);
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
