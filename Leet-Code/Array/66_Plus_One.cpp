534225edead81dbd53a9f20f9eac00f69726877b
tanvir1607 <10viranik@gmail.com>
2026-08-26 11:45:44 +0600

    66. Plus One

---
+++ Leet-Code/Array/66_Plus_One.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int size = digits.size();
+        for (int i = size - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
