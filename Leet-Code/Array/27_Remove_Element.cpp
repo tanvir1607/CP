0f148fbb9ba073fea73f2a03fc324f465eb4569c
tanvir1607 <10viranik@gmail.com>
2026-08-26 10:01:21 +0600

    27. Remove Element

---
+++ Leet-Code/Array/27_Remove_Element.cpp
@@ -0,0 +1,11 @@
+// Solution - 01
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int idx = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) nums[idx++] = nums[i];
+        }
+        return idx;
+    }
+};
