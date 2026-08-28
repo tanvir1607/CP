df63b4ca40c7eee3483cc5e10f0d8de54d7502f1
tanvir1607 <10viranik@gmail.com>
2026-08-28 12:22:53 +0600

    B. Longest Divisors Interval

---
+++ Code-Forces/02-900/B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        ll num;
+        cin >> num;
+
+        int i = 1;
+        while (num % i == 0) i++;
+        cout << --i << endl;
+    }
+
+    return 0;
+}
