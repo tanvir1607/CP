9cac06b60bc6818244830ed80d5912688332015e
tanvir1607 <10viranik@gmail.com>
2026-08-30 15:37:02 +0600

    2529. Maximum Count of Positive Integer and Negative Integer

--- Leet-Code/Binary-Search/2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.cpp
+++ Leet-Code/Binary-Search/2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.cpp
@@ -13,3 +13,24 @@ public:
 
 
 // Solution - 02
+class Solution {
+private:
+    int lower(vector<int> &vec, int target) {
+        int l = 0;
+        int r = vec.size() - 1;
+        int ans = vec.size();
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (vec[mid] >= target) ans = mid, r = mid - 1;
+            else l = mid + 1;
+        }
+        return ans;
+    }
+
+public:
+    int maximumCount(vector<int>& nums) {
+        int cntNeg = lower(nums, 0);
+        int cntPos = nums.size() - lower(nums, 1);
+        return max(cntPos, cntNeg);
+    }
+};
